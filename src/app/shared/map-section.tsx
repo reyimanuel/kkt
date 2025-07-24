5dd39d0be7f41f0a7461927460e33b0187e50e94
Miracle Sumajow <miraclesumajow32@gmail.com>
2025-07-24 20:09:56 +0800

    fix: update map iframe source and correct address details in MapSection component

--- src/app/shared/map-section.tsx
+++ src/app/shared/map-section.tsx
@@ -8,7 +8,7 @@ export function MapSection() {
             <div className="max-w-4xl mx-auto">
                 <div className="bg-white rounded-lg border border-gray-200 shadow-sm overflow-hidden">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15959.567891234567!2d124.6983421!3d1.2721545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x328768426256fe3b%3A0x55802b478e6c1b5b!2sSuluun%20Tiga%2C%20Sulta%2C%20Kabupaten%20Minahasa%20Selatan%2C%20Sulawesi%20Utara!5e0!3m2!1sen!2sid!4v1234567890123!5m2!1sen!2sid"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.640408849188!2d124.69999761929627!3d1.2816239523827007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x328769834c6c61bb%3A0x75d70c47d18e8889!2sDesa%20Suluun%20Tiga!5e0!3m2!1sid!2sid!4v1753358894180!5m2!1sid!2sid"
                         width="100%"
                         height="450"
                         style={{ border: 0 }}
@@ -19,7 +19,7 @@ export function MapSection() {
                 </div>
                 <div className="mt-6 text-center">
                     <p className="text-gray-300 mb-2">
-                        <strong>Alamat:</strong> Suluun Tiga, Sulta, Kabupaten Minahasa Selatan, Sulawesi Utara, Indonesia
+                        <strong>Alamat:</strong> Suluun Tiga, Tareran, Kabupaten Minahasa Selatan, Sulawesi Utara, Indonesia
                     </p>
                     <a
                         href="https://www.google.com/maps/place/Suluun+Tiga,+Sulta,+Kabupaten+Minahasa+Selatan,+Sulawesi+Utara/@1.2721545,124.6983421,15z/data=!3m1!4b1!4m6!3m5!1s0x328768426256fe3b:0x55802b478e6c1b5b!8m2!3d1.2722053!4d124.7105306!16s%2Fg%2F1hc0gkzy0?entry=ttu&g_ep=EgoyMDI1MDcxNi4wIKXMDSoASAFQAw%3D%3D"
